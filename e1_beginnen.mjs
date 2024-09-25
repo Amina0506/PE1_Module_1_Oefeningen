859c15ec2d50f56d606dd6447b35226f070a3da1
Amina0506 <amina010506@gmail.com>
2024-09-25 11:28:48 +0200

    Update e1_beginnen.mjs

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -1,5 +1,5 @@
 // Voeg hier je eigen code in
-
-
-
+let Name = Amina
+let Age = 18
+let favoriteGame = fortnite
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
